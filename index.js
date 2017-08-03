94d1a9dee4c9b3b5d26e0f0ec66a91a724f57be8
Theophilus Omorebee <theo4u@ymail.com>
2017-08-03 01:04:24 +0100

    done with parameters

--- index.js
+++ index.js
@@ -6,9 +6,30 @@ var swaggerDoc = require("./lib/swaggerDoc");
 module.exports = function (sails) {
     return {
 
+        defaults: {
+            __configKey__: {
+                parameters: {},
+                swagger: {
+                    openapi: '',
+                    info: {
+                        title: '',
+                        description: '',
+                        termsOfService: 'http://example.com/terms',
+                        contact: {name: 'Theophilus Omoregbee', url: 'github.com/theo4u', email: 'theo4u@ymail.com'},
+                        license: {name: 'Apache 2.0', url: 'http://www.apache.org/licenses/LICENSE-2.0.html'},
+                        version: '1.0.0'
+                    },
+                    servers: [
+                        {url: 'http://localhost:1337/', description: 'Local server'},
+                        {url: 'http://plutus.glosskode.com', description: 'Development server'}
+                    ],
+                    externalDocs: {}
+                }
+            }
+        },
         // Run when sails loads-- be sure and call `next()`.
         initialize: function (next) {
-            swaggerDoc(sails);
+            swaggerDoc(sails, this);
             return next();
         }
 
