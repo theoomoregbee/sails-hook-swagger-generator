d76ff745d57ddb6630279c36c249f42d54db6bec
Theophilus Omorebee <theo4u@ymail.com>
2017-08-03 17:58:41 +0100

    initial test done for our json creation which seems to work, moving to swagger verification of json

--- index.js
+++ index.js
@@ -11,20 +11,16 @@ module.exports = function (sails) {
                 parameters: {},//we can add up custom parameters here
                 blueprint_parameters: {list: ['']},//we can add custom blueprint action to parameters binding here, any specified overrides default created
                 swagger: {
-                    openapi: '',
+                    swagger: '2.0',
                     info: {
-                        title: '',
-                        description: '',
+                        title: 'Swagger Json',
+                        description: 'This is a generated swagger json for your sails project',
                         termsOfService: 'http://example.com/terms',
                         contact: {name: 'Theophilus Omoregbee', url: 'github.com/theo4u', email: 'theo4u@ymail.com'},
                         license: {name: 'Apache 2.0', url: 'http://www.apache.org/licenses/LICENSE-2.0.html'},
                         version: '1.0.0'
                     },
-                    servers: [
-                        {url: 'http://localhost:1337/', description: 'Local server'},
-                        {url: 'http://plutus.glosskode.com', description: 'Development server'}
-                    ],
-                    externalDocs: {}
+                    externalDocs: {url: 'http://glosskode.com/dev'}
                 }
             }
         },
