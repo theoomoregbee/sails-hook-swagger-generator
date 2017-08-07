187bd3c00a76bf33ba9e2889f7cd7777b24f8120
Theophilus Omorebee <theo4u@ymail.com>
2017-08-07 16:36:55 +0100

    bug fix for parse routes, to remove body from any method other than put and post

--- test/parsers.test.js
+++ test/parsers.test.js
@@ -4,8 +4,6 @@
 var parser = require("../lib/parsers");
 var formatters = require('../lib/type-formatter');
 const chai = require('chai');
-const assertArrays = require('chai-arrays');
-chai.use(assertArrays);
 var assert = chai.assert;
 var expect = chai.expect;
 
@@ -18,7 +16,7 @@ describe('Parser', function () {
         done();
     });
 
-    //for attribute types
+    //for attribute parsing
     describe('Parse Attributes', function () {
         it('should contain', function (done) {
             expect(parser).to.have.property('attributes');
@@ -46,9 +44,7 @@ describe('Parser', function () {
             expect(attributes).to.have.all.keys('properties', 'required');
             expect(Object.keys(attributes)).to.have.lengthOf(2); //must be of length 2 of properties and or required
 
-            // assert.equal(attributes.required, ['name'], 'It should be an array containing the required attributes name or key');
-
-            expect(attributes.required).to.be.containingAllOf(['name']);
+            assert.deepEqual(attributes.required, ['name'], 'It should be an array containing the required attributes name or key');
 
             var expected_properties = {};
             //if it's among our swagger allowed types
@@ -61,10 +57,123 @@ describe('Parser', function () {
             expected_properties['ageLimit'][formatters.validation_type('max')] = model_attributes.ageLimit.max;
             expected_properties['ageLimit'][formatters.validation_type('min')] = model_attributes.ageLimit.min;
 
-            assert.notStrictEqual(expected_properties, attributes.properties, 'It should look exactly like this, following swagger specification');
+            assert.deepEqual(attributes.properties, expected_properties, 'It should look exactly like this, following swagger specification');
 
             done();
         });
     });
 
-});
+    //for route parsing
+    describe('Parse Routes', function () {
+        it('should contain', function (done) {
+            expect(parser).to.have.property('routes');
+            done();
+        });
+
+        describe('should handle the 2 type of route pattern', function () {
+            var expected_route = {
+                user: [{
+                    http_method: 'post',
+                    path: '/user/login',
+                    action: 'login',
+                    keys: [],
+                    summary: '',
+                    description: '',
+                    body: {},
+                    query: []
+                }]
+            };
+
+            it("should handle basic route pattern, 'method path':'controller.action'", function (done) {
+                assert.deepEqual(parser.routes({'post /user/login': 'UserController.login'}), expected_route, "Should return exactly the above properties for each route");
+                done();
+            });
+
+            it("should handle basic route pattern, 'method path':{controller:'UserController', action:'login'}", function (done) {
+
+                assert.deepEqual(parser.routes({
+                    'post /user/login': {
+                        controller: 'UserController',
+                        action: 'login'
+                    }
+                }), expected_route, "Should return exactly the above properties for each route");
+                done();
+            });
+        });
+
+
+        it('should parse path params to keys', function (done) {
+            var route = parser.routes({'put /user/:id/:model': 'UserController.update'});
+            assert.deepEqual(route.user[0].keys, ['id', 'model'], 'It should be an array containing the path param as keys');
+            done();
+        });
+
+        it('should be able to extend swagger object in each route which overrides default route properties', function (done) {
+            var default_route = parser.routes({'post /user/login': 'UserController.login'});
+
+            var actual_parsed_route = parser.routes({
+                'post /user/login': {
+                    controller: 'UserController',
+                    action: 'login',
+                    swagger: {
+                        summary: 'Authentication',
+                        description: 'This is for authentication of any user',
+                        body: {
+                            username: {type: 'string', required: true},
+                            password: {type: 'password', required: true}
+                        },
+                        query: [{$ref: '#/parameters/IDPathParam'}]
+                    }
+                }
+            });
+
+            assert.notDeepEqual(actual_parsed_route, default_route, "should not be same, since swagger object would overrides our default without swagger");
+            done();
+        });
+
+        it('should not contain body in methods other than post and put', function (done) {
+
+            var actual_parsed_route = parser.routes({
+                'post /user/login': {
+                    controller: 'UserController',
+                    action: 'login',
+                    swagger: {
+                        body: {
+                            username: {type: 'string', required: true},
+                            password: {type: 'password', required: true}
+                        },
+                        query: [{$ref: '#/parameters/IDPathParam'}]
+                    }
+                },
+                'put /user/:id': {
+                    controller: 'UserController',
+                    action: 'update',
+                    swagger: {
+                        body: {
+                            name: {type: 'string'}
+                        }
+                    }
+                },
+                'get /user/login': {
+                    controller: 'UserController',
+                    action: 'login',
+                    swagger: {
+                        body: {
+                            names: {type: 'string'}
+                        }
+                    }
+                }
+            });
+
+            expect(actual_parsed_route.user[0], "Since method is post, body is needed").to.have.property('body');
+            expect(actual_parsed_route.user[1], "Since method is put, body is needed").to.have.property('body');
+            expect(actual_parsed_route.user[2], "Since method is get, body is not needed").to.not.have.property('body');
+
+            done();
+        });
+
+    });
+
+
+})
+;
