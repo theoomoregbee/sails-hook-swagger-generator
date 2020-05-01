81c8c9a316ca17486de1afa744e9972ec8cbaba4
Theophilus Omoregbee <theo4u@ymail.com>
2020-05-01 02:09:56 -0400

    test: generateSchemas

--- test/generators.test.ts
+++ test/generators.test.ts
@@ -1,7 +1,9 @@
-import { generateSwaggerPath } from '../lib/generators'
+import { generateSwaggerPath, generateSchemas } from '../lib/generators'
 import { expect } from 'chai';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const userModel = require('../../api/models/User');
 
-describe ('Generators', () => {
+describe('Generators', () => {
 
     describe('generateSwaggerPath', () => {
         it('should return swagger open api version path', done => {
@@ -13,6 +15,42 @@ describe ('Generators', () => {
         it('should return path params as variables', done => {
             const params = generateSwaggerPath('/pets/:id/:anotherID')
             expect(params.variables).to.deep.equal(['id', 'anotherID'])
+            done()
+        })
+    })
+
+    describe('generateSchemas', () => {
+        // TODO: attribute with relationship with other models
+        it('should generate OpenAPIV3 accepted schema type', done => {
+            const actual = generateSchemas({ user: { ...userModel, identity: 'user' } })
+            const expected = {
+                user: {
+                    description: "Sails ORM Model **undefined**",
+                    required: ['names'],
+                    properties: {
+                        id: {
+                            type: "number",
+                            format: "double"
+                        },
+                        names: {
+                            type: "string",
+                            example: "First Middle Last"
+                        },
+                        email: {
+                            type: "string",
+                            description: "Just any old email"
+                        },
+                        sex: {
+                            type: "string"
+                        },
+                        ageLimit: {
+                            type: "number",
+                            format: "double"
+                        }
+                    }
+                }
+            }
+            expect(actual).to.deep.equal(expected)
             done()
         })
     })
